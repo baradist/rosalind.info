68af53e7e30bbc4c3915db614f92434b8f3afd9e
baradist <baradist@ya.ru>
2016-06-26 16:17:00 +0300

    fix modules' names

---
+++ bioinf-stronghold/src/main/java/DNA_counting_nucleotides/Main.java
@@ -0,0 +1,14 @@
+package DNA_counting_nucleotides;
+
+import java.util.Scanner;
+
+/**
+ * Created by Oleg Grigorjev on 26.06.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        NucleotidesCounter counter = new NucleotidesCounter(scanner.next());
+        System.out.println(counter.getResult());
+    }
+}
---
+++ bioinf-stronghold/src/main/java/DNA_counting_nucleotides/NucleotidesCounter.java
@@ -0,0 +1,39 @@
+package DNA_counting_nucleotides;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by Oleg Grigorjev on 26.06.2016.
+ */
+public class NucleotidesCounter {
+    private Map<Character, Integer> nucleotides;
+
+    public NucleotidesCounter() {
+        this.nucleotides = new TreeMap<>();
+    }
+
+    public NucleotidesCounter(String dataset) {
+        this();
+        countSymbols(dataset);
+    }
+
+    public String getResult() {
+        String result = "";
+        for (Integer integer : nucleotides.values()) {
+            result += (integer + " ");
+        }
+        return result.substring(0, result.length() - 1);
+    }
+
+    private void countSymbols(String dataset) {
+        for (int i = 0; i < dataset.length(); i++) {
+            char charAt = dataset.charAt(i);
+            if (nucleotides.containsKey(charAt)) {
+                nucleotides.put(charAt, nucleotides.get(charAt) + 1);
+            } else {
+                nucleotides.put(charAt, 1);
+            }
+        }
+    }
+}
